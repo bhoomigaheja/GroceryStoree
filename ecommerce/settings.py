bb77b4ea2eaab8182b9575610bc3bd7af7b1e320
bhoomi gaheja <bhoomigaheja.ps12@gmail.com>
2024-05-19 13:10:43 +0530

    first commit

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -1,3 +1,4 @@
+
 """
 Django settings for ecommerce project.
 
@@ -23,7 +24,7 @@ STATIC_DIR=os.path.join(BASE_DIR,'static')
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#vw(03o=(9kbvg!&2d5i!2$_58x@_-3l4wujpow6(ym37jxnza'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
